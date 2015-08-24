8cdc96861f32d0a021842928ae5cdc1791aa2171
KittyGarrison <katrina.garrison@detroitlabs.com>
2015-08-24 16:05:23 -0400

    orderBy choices

--- app.js
+++ app.js
@@ -7,12 +7,28 @@
     function mainController() {
         vm = this;
 
-        vm.tasks = [];
+        vm.tasks = [
+          {name : "this", isComplete : false, createdOn : 1440446404530},
+          {name : "that", isComplete : false, createdOn : 1440446404540},
+          {name : "zed", isComplete : false, createdOn : 1440446404550},
+          {name : "buzz", isComplete : false, createdOn : 1440446404560},
+          {name : "fizz", isComplete : false, createdOn : 1440446404569}
+        ];
         vm.addTask = addTask;
-        vm.name = "task"
+        vm.orderBy = "";
+        vm.changeOrder = changeOrder;
+        vm.name = "";
 
         function addTask(){
-          vm.tasks.push({name : vm.name, isComplete : false})
+          if (!vm.name) {
+            return
+          };
+          vm.tasks.push({name : vm.name, isComplete : false, createdOn : Date.now()});
+          vm.name = '';
+        }
+
+        function changeOrder(order){
+          vm.orderBy = order;
         }
 
     }
