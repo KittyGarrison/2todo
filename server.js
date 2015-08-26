45fbeaccf05d2c0b3dcdd228fff64d5f117d5a0b
KittyGarrison <katrina.garrison@detroitlabs.com>
2015-08-25 21:50:12 -0400

    server and CRD done

--- server.js
+++ server.js
@@ -14,5 +14,56 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+jason'}));
 app.use(methodOverride());
 
+var Task = mongoose.model('Task', {
+  text : String,
+  isDone : Boolean
+})
+
+app.get('/api/tasks', function(req, res) {
+  Task.find(function(err, tasks) {
+    if (err){
+      res.send(err)
+    }
+    res.json(tasks);
+  });
+});
+
+app.post('/api/tasks', function(req, res) {
+
+  Task.create({
+    text : req.body.text,
+    isDone : false
+  }, function(err, tasks) {
+    if (err){
+      res.send(err)
+    };
+    Task.find(function(err, tasks){
+      if (err){
+        res.send(err)
+      };
+      res.json(tasks);  
+    });
+  });
+});
+
+app.delete('/api/tasks/:task_id', function(req, res) {
+  Task.remove({
+    _id : req.params.taks_id
+  }, function(err, tasks) {
+    if (err){
+        res.send(err)
+      };
+
+    Task.find(function(err, tasks){
+      if (err){
+        res.send(err)
+      };
+      res.json(tasks);
+    });
+  });
+});
+
+
 app.listen(8080);
 console.log("App listening on port 8080")
+
